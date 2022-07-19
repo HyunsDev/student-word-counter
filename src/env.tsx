b546cc9b0ce92cc40bf47bb8751a1c00c1e6e90a
Donghyeon Kim <0916dhkim@gmail.com>
2022-07-18 23:46:39 -0400

    Replace mandatory to invariants

--- src/env.tsx
+++ src/env.tsx
@@ -1,21 +1,32 @@
-/**
- * For asserting that a value exists
- * @param value a value to be checked
- * @returns value if it exists
- * @throws if value is undefined
- */
-function mandatory<T>(value: T | undefined) {
-  if (value === undefined) {
-    throw new Error("Value is undefined");
-  }
-  return value;
-}
+import invariant from "tiny-invariant";
 
-export const API_KEY = mandatory(process.env.REACT_APP_API_KEY);
-export const AUTH_DOMAIN = mandatory(process.env.REACT_APP_AUTH_DOMAIN);
-export const PROJECT_ID = mandatory(process.env.REACT_APP_PROJECT_ID);
-export const STORAGE_BUCKET = mandatory(process.env.REACT_APP_STORAGE_BUCKET);
-export const MESSAGING_SENDER_ID = mandatory(
-  process.env.REACT_APP_MESSAGING_SENDER_ID
+invariant(process.env.REACT_APP_API_KEY, "REACT_APP_API_KEY is not defined");
+export const API_KEY: string = process.env.REACT_APP_API_KEY;
+
+invariant(
+  process.env.REACT_APP_AUTH_DOMAIN,
+  "REACT_APP_AUTH_DOMAIN is not defined"
 );
-export const APP_ID = mandatory(process.env.REACT_APP_APP_ID);
+export const AUTH_DOMAIN: string = process.env.REACT_APP_AUTH_DOMAIN;
+
+invariant(
+  process.env.REACT_APP_PROJECT_ID,
+  "REACT_APP_PROJECT_ID is not defined"
+);
+export const PROJECT_ID: string = process.env.REACT_APP_PROJECT_ID;
+
+invariant(
+  process.env.REACT_APP_STORAGE_BUCKET,
+  "REACT_APP_STORAGE_BUCKET is not defined"
+);
+export const STORAGE_BUCKET: string = process.env.REACT_APP_STORAGE_BUCKET;
+
+invariant(
+  process.env.REACT_APP_MESSAGING_SENDER_ID,
+  "REACT_APP_MESSAGING_SENDER_ID is not defined"
+);
+export const MESSAGING_SENDER_ID: string =
+  process.env.REACT_APP_MESSAGING_SENDER_ID;
+
+invariant(process.env.REACT_APP_APP_ID, "REACT_APP_APP_ID is not defined");
+export const APP_ID: string = process.env.REACT_APP_APP_ID;
